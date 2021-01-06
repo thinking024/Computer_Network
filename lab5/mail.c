9883f06ec3680300a450a95f841279a76a859568
YiyuanZhu <2643811975@qq.com>
2021-01-06 22:56:08 +0800

    send group email

--- lab5/mail.c
+++ lab5/mail.c
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include <Winsock2.h>
+#include <string.h>
 
 #pragma comment(lib, "WS2_32.lib")
 struct data6
@@ -14,16 +15,183 @@ struct data6
 char con628(char c6);
 void base64(char *dbuf, char *buf128, int len);
 void sendemail(char *email, char *body);
-int open_socket(struct sockaddr *addr);
 
 int main()
 {
-    char email[] = "3072230851@qq.com";
+    //sendemail(email, body);
+
+    struct sockaddr_in smtpServer = {0};
+    char sendBuffer[1500] = {0};
+    char rcvBuffer[1500] = {0};
+    WSADATA WSAData;
+    if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
+    {
+        fprintf(stderr, "Init error!\n");
+        exit(-1);
+    }
+    
+    memset(&smtpServer, 0, sizeof(smtpServer));
+    smtpServer.sin_family = AF_INET;
+    smtpServer.sin_port = htons(25);
+    HOSTENT* serverInfo = gethostbyname("smtp.qq.com");
+    smtpServer.sin_addr.s_addr = *((DWORD*)serverInfo->h_addr_list[0]); //qq smtp 服务器
+
+    int mailSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (mailSocket < 0)
+    {
+        fprintf(stderr, "Open socket(TCP) error!\n");
+        exit(-1);
+    }
+    if (connect(mailSocket, (struct sockaddr *)&smtpServer, sizeof(struct sockaddr)) < 0)
+    {
+        fprintf(stderr, "Connect socket(TCP) error!\n");
+        exit(-1);
+    }
+    memset(rcvBuffer, 0, 1500);
+    if (recv(mailSocket, rcvBuffer, 1500, 0) == 0)
+    {
+        fprintf(stderr, "fail to receive!\n");
+        exit(-1);
+    }
+
+    printf("connect: %s\n", rcvBuffer);
+
+    // EHLO
+    char localHost[20] = {0};
+    gethostname(localHost, 20);
+    memset(sendBuffer, 0, 1500);
+    strcat(localHost, "-PC\r\n");
+    char ehlo[1500] = {0};
+    strcat(ehlo, "EHLO ");
+    sprintf(sendBuffer, strcat(ehlo,localHost));
+    // printf("%s\n", sendBuffer);
+
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("EHLO: %s\n", rcvBuffer);
+
+    // AUTH LOGIN
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "AUTH LOGIN\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    // printf("%s\n", buf);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("AUTH: %s\n", rcvBuffer);
+
+    // USER
+    char emailFrom[128];
+    printf("input your qq: ");
+    scanf("%s", emailFrom);
+    char qq[128] = {0};
+    strcpy(qq, emailFrom);
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, emailFrom); //你的qq号
+    memset(emailFrom, 0, sizeof(emailFrom));
+    base64(emailFrom, sendBuffer, strlen(sendBuffer));
+    sprintf(sendBuffer, "%s\r\n", emailFrom);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    // printf("%s\n", buf);
+    memset(rcvBuffer, 0, sizeof(rcvBuffer));
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("user: %s\n", rcvBuffer);
+
+    // PASSWORD
+    char password[128];
+    printf("input your auth code: ");
+    scanf("%s", password);
+
+    // "bpgblkyafitiebga"
+    sprintf(sendBuffer, password); //使用授权码
+    memset(password, 0, sizeof(password));
+    base64(password, sendBuffer, strlen(sendBuffer));
+    sprintf(sendBuffer, "%s\r\n", password);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    // printf("%s\n", buf);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("password: %s\n", rcvBuffer);
+
+    // MAIL FROM
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "MAIL FROM: <%s@qq.com>\r\n", qq);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("mail from: %s\n", rcvBuffer);
+
+
+    // RCPT TO
+    char emailTo[50];
+    printf("input receive email address, split with space, ends with #: ");
+    scanf("%s", emailTo);
+
+    while (strcmp("#", emailTo) != 0)
+    {
+        sprintf(sendBuffer, "RCPT TO:<%s>\r\n", emailTo);
+        send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+        memset(rcvBuffer, 0, 1500);
+        recv(mailSocket, rcvBuffer, 1500, 0);
+        printf("mail to: %s\n", rcvBuffer);
+        scanf("%s", emailTo);
+    }
+    
+
+    // DATA 准备开始发送邮件内容
+    sprintf(sendBuffer, "DATA\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("data begin: %s\n", rcvBuffer);
+
+    char subject[50];
+    printf("input your email subject: ");
+    getchar();
+    //scanf("%s", subject);
+    gets(subject);
+
+    char content[500];
+    printf("input your email content: ");
+    //scanf("%s", content);
+    gets(content);
+
     char body[] = "From: \"lucy\"<2643811975@qq.com>\r\n"
                   "To: \"dasiy\"<3072230851@qq.com>\r\n"
                   "Subject: Hello\r\n\r\n"
                   "Hello World, Hello Email!";
-    sendemail(email, body);
+    
+    char from[200] = {0};
+    strcat(from, "From: \"");
+    strcat(from, qq);
+    strcat(from, "\"<");
+    strcat(from, qq);
+    strcat(from, "@qq.com>\r\n");
+
+    char emailBody[10000] = {0};
+    //strcat(emailBody, from);
+    strcat(emailBody, "Subject: ");
+    strcat(emailBody, subject);
+    strcat(emailBody, "\r\n\r\n");
+    strcat(emailBody, content);
+
+    // 发送邮件内容，\r\n.\r\n内容结束标记
+    sprintf(sendBuffer, "%s\r\n.\r\n", emailBody);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("data end: %s\n", rcvBuffer);
+
+    // QUIT
+    sprintf(sendBuffer, "QUIT\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("quit: %s\n", rcvBuffer);
+
+    // VC2005 需要使用
+    closesocket(mailSocket);
+    WSACleanup();
     return 0;
 }
 
@@ -94,126 +262,130 @@ void base64(char *dbuf, char *buf128, int len)
 // 发送邮件
 void sendemail(char *email, char *body)
 {
-    int sockfd = 0;
-    struct sockaddr_in their_addr = {0};
-    char buf[1500] = {0};
-    char rbuf[1500] = {0};
-    char login[128] = {0};
+    struct sockaddr_in smtpServer = {0};
+    char sendBuffer[1500] = {0};
+    char rcvBuffer[1500] = {0};
+    char account[128] = {0};
     char pass[128] = {0};
     WSADATA WSAData;
-    WSAStartup(MAKEWORD(2, 2), &WSAData);
-    memset(&their_addr, 0, sizeof(their_addr));
-    their_addr.sin_family = AF_INET;
-    their_addr.sin_port = htons(25);
-    their_addr.sin_addr.s_addr = inet_addr("120.241.25.80"); //qq smtp 服务器
-    // 连接邮件服务器，如果连接后没有响应，则2 秒后重新连接
-    sockfd = open_socket((struct sockaddr *)&their_addr);
-    memset(rbuf, 0, 1500);
-    while (recv(sockfd, rbuf, 1500, 0) == 0)
+    if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
     {
-        printf("reconnect...\n");
-        sockfd = open_socket((struct sockaddr *)&their_addr);
-        memset(rbuf, 0, 1500);
+        fprintf(stderr, "Init error!\n");
+        exit(-1);
+    }
+    
+    memset(&smtpServer, 0, sizeof(smtpServer));
+    smtpServer.sin_family = AF_INET;
+    smtpServer.sin_port = htons(25);
+    //smtpServer.sin_addr.s_addr = inet_addr("120.241.25.80"); //qq smtp 服务器
+    HOSTENT* serverInfo = gethostbyname("smtp.qq.com");
+    smtpServer.sin_addr.s_addr = *((DWORD*)serverInfo->h_addr_list[0]); //qq smtp 服务器
+
+    int mailSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (mailSocket < 0)
+    {
+        fprintf(stderr, "Open socket(TCP) error!\n");
+        exit(-1);
+    }
+    if (connect(mailSocket, (struct sockaddr *)&smtpServer, sizeof(struct sockaddr)) < 0)
+    {
+        fprintf(stderr, "Connect socket(TCP) error!\n");
+        exit(-1);
+    }
+    memset(rcvBuffer, 0, 1500);
+    if (recv(mailSocket, rcvBuffer, 1500, 0) == 0)
+    {
+        fprintf(stderr, "fail to receive!\n");
+        exit(-1);
     }
 
-    printf("connect: %s\n", rbuf);
+    printf("connect: %s\n", rcvBuffer);
 
     // EHLO
-    memset(buf, 0, 1500);
-    sprintf(buf, "EHLO abcdefg-PC\r\n");
-    send(sockfd, buf, strlen(buf), 0);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("EHLO: %s\n", rbuf);
+    char localHost[20] = {0};
+    gethostname(localHost, 20);
+    memset(sendBuffer, 0, 1500);
+    strcat(localHost, "-PC\r\n");
+    char ehlo[1500] = {0};
+    strcat(ehlo, "EHLO ");
+    sprintf(sendBuffer, strcat(ehlo,localHost));
+    printf("%s\n", sendBuffer);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("EHLO: %s\n", rcvBuffer);
 
     // AUTH LOGIN
-    memset(buf, 0, 1500);
-    sprintf(buf, "AUTH LOGIN\r\n");
-    send(sockfd, buf, strlen(buf), 0);
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "AUTH LOGIN\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
     // printf("%s\n", buf);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("AUTH: %s\n", rbuf);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("AUTH: %s\n", rcvBuffer);
 
     // USER
-    memset(buf, 0, 1500);
-    sprintf(buf, "2643811975"); //你的qq号
-    memset(login, 0, 128);
-    base64(login, buf, strlen(buf));
-    sprintf(buf, "%s\r\n", login);
-    send(sockfd, buf, strlen(buf), 0);
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "2643811975"); //你的qq号
+    memset(account, 0, 128);
+    base64(account, sendBuffer, strlen(sendBuffer));
+    sprintf(sendBuffer, "%s\r\n", account);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
     // printf("%s\n", buf);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("user: %s\n", rbuf);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("user: %s\n", rcvBuffer);
 
     // PASSWORD
-    sprintf(buf, "bpgblkyafitiebga"); //使用授权码
+    sprintf(sendBuffer, "bpgblkyafitiebga"); //使用授权码
     memset(pass, 0, 128);
-    base64(pass, buf, strlen(buf));
-    sprintf(buf, "%s\r\n", pass);
-    send(sockfd, buf, strlen(buf), 0);
+    base64(pass, sendBuffer, strlen(sendBuffer));
+    sprintf(sendBuffer, "%s\r\n", pass);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
     // printf("%s\n", buf);
 
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("password: %s\n", rbuf);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("password: %s\n", rcvBuffer);
 
     // MAIL FROM
-    memset(buf, 0, 1500);
-    sprintf(buf, "MAIL FROM: <2643811975@qq.com>\r\n");
-    send(sockfd, buf, strlen(buf), 0);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("mail from: %s\n", rbuf);
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "MAIL FROM: <2643811975@qq.com>\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("mail from: %s\n", rcvBuffer);
 
     // RCPT TO 第一个收件人
-    sprintf(buf, "RCPT TO:<%s>\r\n", email);
-    send(sockfd, buf, strlen(buf), 0);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("mail to: %s\n", rbuf);
+    sprintf(sendBuffer, "RCPT TO:<%s>\r\n", email);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("mail to: %s\n", rcvBuffer);
 
     // DATA 准备开始发送邮件内容
-    sprintf(buf, "DATA\r\n");
-    send(sockfd, buf, strlen(buf), 0);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("data begin: %s\n", rbuf);
+    sprintf(sendBuffer, "DATA\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("data begin: %s\n", rcvBuffer);
 
     // 发送邮件内容，\r\n.\r\n内容结束标记
-    sprintf(buf, "%s\r\n.\r\n", body);
-    send(sockfd, buf, strlen(buf), 0);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("data end: %s\n", rbuf);
+    sprintf(sendBuffer, "%s\r\n.\r\n", body);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("data end: %s\n", rcvBuffer);
 
     // QUIT
-    sprintf(buf, "QUIT\r\n");
-    send(sockfd, buf, strlen(buf), 0);
-    memset(rbuf, 0, 1500);
-    recv(sockfd, rbuf, 1500, 0);
-    printf("quit: %s\n", rbuf);
+    sprintf(sendBuffer, "QUIT\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("quit: %s\n", rcvBuffer);
 
     // VC2005 需要使用
-    closesocket(sockfd);
+    closesocket(mailSocket);
     WSACleanup();
-    return 0;
-}
-// 打开TCP Socket连接
-int open_socket(struct sockaddr *addr)
-{
-    int sockfd = 0;
-    sockfd = socket(PF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        fprintf(stderr, "Open sockfd(TCP) error!\n");
-        exit(-1);
-    }
-    if (connect(sockfd, addr, sizeof(struct sockaddr)) < 0)
-    {
-        fprintf(stderr, "Connect sockfd(TCP) error!\n");
-        exit(-1);
-    }
-    return sockfd;
+    return;
 }
