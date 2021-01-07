415ad82beec1f4b9e13cb7e1630ba8c04823f898
YiyuanZhu <2643811975@qq.com>
2021-01-08 00:08:17 +0800

    finish http proxy

--- lab5/mail.c
+++ lab5/mail.c
@@ -1,6 +1,6 @@
+#include <Winsock2.h>
 #include <windows.h>
 #include <stdio.h>
-#include <Winsock2.h>
 #include <string.h>
 
 #pragma comment(lib, "WS2_32.lib")
@@ -13,58 +13,221 @@ struct data6
 };
 // 协议中加密部分使用的是base64方法
 char con628(char c6);
-void base64(char *dbuf, char *buf128, int len);
-void sendemail(char *email, char *body);
+void base64(char* dbuf, char* buf128, int len);
+void sendEmail(char* email, char* body);
+void receiveEmail(char* account, char* password);
+int connectServer(char* ip, int port);
 
 int main()
 {
     //sendemail(email, body);
+    char qq[128];
+    printf("input your qq: ");
+    scanf("%s", qq);
+    char authcode[128];
+    printf("input your auth code: ");
+    scanf("%s", authcode);
 
-    struct sockaddr_in smtpServer = {0};
-    char sendBuffer[1500] = {0};
-    char rcvBuffer[1500] = {0};
+    char cmd[20] = { 0 };
+    printf("send or receive email: ");
+    while (strcmp(cmd, "#") != 0)
+    {
+        scanf("%s", cmd);
+        if (strcmp(cmd, "send") == 0)
+        {
+            char account[20];
+            memset(account, 0, sizeof(account));
+            sprintf(account, qq);
+            char password[128] = { 0 };
+            memset(password, 0, sizeof(password));
+            strcpy(password, authcode);
+
+            sendEmail(account, password);
+        }
+        if (strcmp(cmd, "receive") == 0)
+        {
+            char account2[20];
+            memset(account2, 0, 20);
+            sprintf(account2, qq);
+            char password2[128] = { 0 };
+            memset(password2, 0, 20);
+            strcpy(password2, authcode);
+
+            receiveEmail(account2, password2);
+        }
+    }
+
+    return 0;
+}
+
+char con628(char c6)
+{
+    char rtn = '\0';
+    if (c6 < 26)
+        rtn = c6 + 65;
+    else if (c6 < 52)
+        rtn = c6 + 71;
+    else if (c6 < 62)
+        rtn = c6 - 4;
+    else if (c6 == 62)
+        rtn = 43;
+    else
+        rtn = 47;
+    return rtn;
+}
+
+// base64的实现
+void base64(char* dbuf, char* buf128, int len)
+{
+    struct data6* ddd = NULL;
+    int i = 0;
+    char buf[256] = { 0 };
+    char* tmp = NULL;
+    char cc = '\0';
+    memset(buf, 0, 256);
+    strcpy(buf, buf128);
+    for (i = 1; i <= len / 3; i++)
+    {
+        tmp = buf + (i - 1) * 3;
+        cc = tmp[2];
+        tmp[2] = tmp[0];
+        tmp[0] = cc;
+        ddd = (struct data6*)tmp;
+        dbuf[(i - 1) * 4 + 0] = con628((unsigned int)ddd->d1);
+        dbuf[(i - 1) * 4 + 1] = con628((unsigned int)ddd->d2);
+        dbuf[(i - 1) * 4 + 2] = con628((unsigned int)ddd->d3);
+        dbuf[(i - 1) * 4 + 3] = con628((unsigned int)ddd->d4);
+    }
+    if (len % 3 == 1)
+    {
+        tmp = buf + (i - 1) * 3;
+        cc = tmp[2];
+        tmp[2] = tmp[0];
+        tmp[0] = cc;
+        ddd = (struct data6*)tmp;
+        dbuf[(i - 1) * 4 + 0] = con628((unsigned int)ddd->d1);
+        dbuf[(i - 1) * 4 + 1] = con628((unsigned int)ddd->d2);
+        dbuf[(i - 1) * 4 + 2] = '=';
+        dbuf[(i - 1) * 4 + 3] = '=';
+    }
+    if (len % 3 == 2)
+    {
+        tmp = buf + (i - 1) * 3;
+        cc = tmp[2];
+        tmp[2] = tmp[0];
+        tmp[0] = cc;
+        ddd = (struct data6*)tmp;
+        dbuf[(i - 1) * 4 + 0] = con628((unsigned int)ddd->d1);
+        dbuf[(i - 1) * 4 + 1] = con628((unsigned int)ddd->d2);
+        dbuf[(i - 1) * 4 + 2] = con628((unsigned int)ddd->d3);
+        dbuf[(i - 1) * 4 + 3] = '=';
+    }
+    return;
+}
+
+int connectServer(char* ip, int port)
+{
+    struct sockaddr_in qqServer = { 0 };
+    char sendBuffer[1500] = { 0 };
+    char rcvBuffer[1500] = { 0 };
     WSADATA WSAData;
     if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
     {
         fprintf(stderr, "Init error!\n");
         exit(-1);
     }
-    
-    memset(&smtpServer, 0, sizeof(smtpServer));
-    smtpServer.sin_family = AF_INET;
-    smtpServer.sin_port = htons(25);
-    HOSTENT* serverInfo = gethostbyname("smtp.qq.com");
-    smtpServer.sin_addr.s_addr = *((DWORD*)serverInfo->h_addr_list[0]); //qq smtp 服务器
+
+    memset(&qqServer, 0, sizeof(qqServer));
+    qqServer.sin_family = AF_INET;
+    qqServer.sin_port = htons(port);
+    HOSTENT* serverInfo = gethostbyname(ip);
+    qqServer.sin_addr.s_addr = *((DWORD*)serverInfo->h_addr_list[0]); //qq smtp 服务器
 
     int mailSocket = socket(PF_INET, SOCK_STREAM, 0);
     if (mailSocket < 0)
     {
         fprintf(stderr, "Open socket(TCP) error!\n");
-        exit(-1);
+        return -1;
     }
-    if (connect(mailSocket, (struct sockaddr *)&smtpServer, sizeof(struct sockaddr)) < 0)
+    if (connect(mailSocket, (struct sockaddr*)&qqServer, sizeof(struct sockaddr)) < 0)
     {
         fprintf(stderr, "Connect socket(TCP) error!\n");
-        exit(-1);
+        return -1;
     }
     memset(rcvBuffer, 0, 1500);
     if (recv(mailSocket, rcvBuffer, 1500, 0) == 0)
     {
         fprintf(stderr, "fail to receive!\n");
-        exit(-1);
+        return -1;
     }
-
     printf("connect: %s\n", rcvBuffer);
+    return mailSocket;
+}
+
+// 收取邮件
+void receiveEmail(char* acount, char* password)
+{
+    char sendBuffer[1500] = { 0 };
+    char rcvBuffer[1500] = { 0 };
+    int mailSocket = connectServer((char *)"pop.qq.com", 110);
+
+    // user
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "user %s\r\n", acount);
+
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("user: %s\n", rcvBuffer);
+
+    // password 
+    memset(sendBuffer, 0, 1500);
+    sprintf(sendBuffer, "pass %s\r\n", password);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("pass: %s\n", rcvBuffer);
+
+    int emailNo;
+    printf("input order of the emial you want to receive: ");
+    scanf("%d", &emailNo);
+    sprintf(sendBuffer, "retr %d\r\n", emailNo);
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    int rcv;
+    do
+    {
+        memset(rcvBuffer, 0, sizeof(rcvBuffer));
+        // rcv = recv(mailSocket, rcvBuffer, 1500, 0);
+        if ((rcv = recv(mailSocket, rcvBuffer, 1500, 0)) > 0)
+        {
+            printf("%s", rcvBuffer);
+        }
+    } while (rcv >= 1500);
+
+    // QUIT
+    sprintf(sendBuffer, "quit\r\n");
+    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
+    memset(rcvBuffer, 0, 1500);
+    recv(mailSocket, rcvBuffer, 1500, 0);
+    printf("quit: %s\n", rcvBuffer);
+
+    closesocket(mailSocket);
+    WSACleanup();
+    return;
+}
+
+// 发送邮件
+void sendEmail(char* account, char* password)
+{
+    char sendBuffer[1500] = { 0 };
+    char rcvBuffer[1500] = { 0 };
+    int mailSocket = connectServer((char *)"smtp.qq.com", 25);
 
     // EHLO
-    char localHost[20] = {0};
+    char localHost[20] = { 0 };
     gethostname(localHost, 20);
     memset(sendBuffer, 0, 1500);
-    strcat(localHost, "-PC\r\n");
-    char ehlo[1500] = {0};
-    strcat(ehlo, "EHLO ");
-    sprintf(sendBuffer, strcat(ehlo,localHost));
-    // printf("%s\n", sendBuffer);
+    sprintf(sendBuffer, "EHLO %s-PC\r\n", localHost);
 
     send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
     memset(rcvBuffer, 0, 1500);
@@ -81,30 +244,22 @@ int main()
     printf("AUTH: %s\n", rcvBuffer);
 
     // USER
-    char emailFrom[128];
-    printf("input your qq: ");
-    scanf("%s", emailFrom);
-    char qq[128] = {0};
-    strcpy(qq, emailFrom);
+    char qq[128] = { 0 };
+    strcpy(qq, account);
     memset(sendBuffer, 0, 1500);
-    sprintf(sendBuffer, emailFrom); //你的qq号
-    memset(emailFrom, 0, sizeof(emailFrom));
-    base64(emailFrom, sendBuffer, strlen(sendBuffer));
-    sprintf(sendBuffer, "%s\r\n", emailFrom);
+    sprintf(sendBuffer, account); //你的qq号
+    memset(account, 0, sizeof(account));
+    base64(account, sendBuffer, strlen(sendBuffer));
+    sprintf(sendBuffer, "%s\r\n", account);
     send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    // printf("%s\n", buf);
     memset(rcvBuffer, 0, sizeof(rcvBuffer));
     recv(mailSocket, rcvBuffer, 1500, 0);
     printf("user: %s\n", rcvBuffer);
 
     // PASSWORD
-    char password[128];
-    printf("input your auth code: ");
-    scanf("%s", password);
-
     // "bpgblkyafitiebga"
     sprintf(sendBuffer, password); //使用授权码
-    memset(password, 0, sizeof(password));
+    memset(password, 0, 128);
     base64(password, sendBuffer, strlen(sendBuffer));
     sprintf(sendBuffer, "%s\r\n", password);
     send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
@@ -136,7 +291,7 @@ int main()
         printf("mail to: %s\n", rcvBuffer);
         scanf("%s", emailTo);
     }
-    
+
 
     // DATA 准备开始发送邮件内容
     sprintf(sendBuffer, "DATA\r\n");
@@ -157,18 +312,18 @@ int main()
     gets(content);
 
     char body[] = "From: \"lucy\"<2643811975@qq.com>\r\n"
-                  "To: \"dasiy\"<3072230851@qq.com>\r\n"
-                  "Subject: Hello\r\n\r\n"
-                  "Hello World, Hello Email!";
-    
-    char from[200] = {0};
+        "To: \"dasiy\"<3072230851@qq.com>\r\n"
+        "Subject: Hello\r\n\r\n"
+        "Hello World, Hello Email!";
+
+    char from[200] = { 0 };
     strcat(from, "From: \"");
     strcat(from, qq);
     strcat(from, "\"<");
     strcat(from, qq);
     strcat(from, "@qq.com>\r\n");
 
-    char emailBody[10000] = {0};
+    char emailBody[10000] = { 0 };
     //strcat(emailBody, from);
     strcat(emailBody, "Subject: ");
     strcat(emailBody, subject);
@@ -176,7 +331,7 @@ int main()
     strcat(emailBody, content);
 
     // 发送邮件内容，\r\n.\r\n内容结束标记
-    sprintf(sendBuffer, "%s\r\n.\r\n", emailBody);
+    sprintf(sendBuffer, "Subject: %s\r\n\r\n%s\r\n.\r\n", subject, content);
     send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
     memset(rcvBuffer, 0, 1500);
     recv(mailSocket, rcvBuffer, 1500, 0);
@@ -188,203 +343,6 @@ int main()
     memset(rcvBuffer, 0, 1500);
     recv(mailSocket, rcvBuffer, 1500, 0);
     printf("quit: %s\n", rcvBuffer);
-
-    // VC2005 需要使用
-    closesocket(mailSocket);
-    WSACleanup();
-    return 0;
-}
-
-char con628(char c6)
-{
-    char rtn = '\0';
-    if (c6 < 26)
-        rtn = c6 + 65;
-    else if (c6 < 52)
-        rtn = c6 + 71;
-    else if (c6 < 62)
-        rtn = c6 - 4;
-    else if (c6 == 62)
-        rtn = 43;
-    else
-        rtn = 47;
-    return rtn;
-}
-
-// base64的实现
-void base64(char *dbuf, char *buf128, int len)
-{
-    struct data6 *ddd = NULL;
-    int i = 0;
-    char buf[256] = {0};
-    char *tmp = NULL;
-    char cc = '\0';
-    memset(buf, 0, 256);
-    strcpy(buf, buf128);
-    for (i = 1; i <= len / 3; i++)
-    {
-        tmp = buf + (i - 1) * 3;
-        cc = tmp[2];
-        tmp[2] = tmp[0];
-        tmp[0] = cc;
-        ddd = (struct data6 *)tmp;
-        dbuf[(i - 1) * 4 + 0] = con628((unsigned int)ddd->d1);
-        dbuf[(i - 1) * 4 + 1] = con628((unsigned int)ddd->d2);
-        dbuf[(i - 1) * 4 + 2] = con628((unsigned int)ddd->d3);
-        dbuf[(i - 1) * 4 + 3] = con628((unsigned int)ddd->d4);
-    }
-    if (len % 3 == 1)
-    {
-        tmp = buf + (i - 1) * 3;
-        cc = tmp[2];
-        tmp[2] = tmp[0];
-        tmp[0] = cc;
-        ddd = (struct data6 *)tmp;
-        dbuf[(i - 1) * 4 + 0] = con628((unsigned int)ddd->d1);
-        dbuf[(i - 1) * 4 + 1] = con628((unsigned int)ddd->d2);
-        dbuf[(i - 1) * 4 + 2] = '=';
-        dbuf[(i - 1) * 4 + 3] = '=';
-    }
-    if (len % 3 == 2)
-    {
-        tmp = buf + (i - 1) * 3;
-        cc = tmp[2];
-        tmp[2] = tmp[0];
-        tmp[0] = cc;
-        ddd = (struct data6 *)tmp;
-        dbuf[(i - 1) * 4 + 0] = con628((unsigned int)ddd->d1);
-        dbuf[(i - 1) * 4 + 1] = con628((unsigned int)ddd->d2);
-        dbuf[(i - 1) * 4 + 2] = con628((unsigned int)ddd->d3);
-        dbuf[(i - 1) * 4 + 3] = '=';
-    }
-    return;
-}
-// 发送邮件
-void sendemail(char *email, char *body)
-{
-    struct sockaddr_in smtpServer = {0};
-    char sendBuffer[1500] = {0};
-    char rcvBuffer[1500] = {0};
-    char account[128] = {0};
-    char pass[128] = {0};
-    WSADATA WSAData;
-    if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
-    {
-        fprintf(stderr, "Init error!\n");
-        exit(-1);
-    }
-    
-    memset(&smtpServer, 0, sizeof(smtpServer));
-    smtpServer.sin_family = AF_INET;
-    smtpServer.sin_port = htons(25);
-    //smtpServer.sin_addr.s_addr = inet_addr("120.241.25.80"); //qq smtp 服务器
-    HOSTENT* serverInfo = gethostbyname("smtp.qq.com");
-    smtpServer.sin_addr.s_addr = *((DWORD*)serverInfo->h_addr_list[0]); //qq smtp 服务器
-
-    int mailSocket = socket(PF_INET, SOCK_STREAM, 0);
-    if (mailSocket < 0)
-    {
-        fprintf(stderr, "Open socket(TCP) error!\n");
-        exit(-1);
-    }
-    if (connect(mailSocket, (struct sockaddr *)&smtpServer, sizeof(struct sockaddr)) < 0)
-    {
-        fprintf(stderr, "Connect socket(TCP) error!\n");
-        exit(-1);
-    }
-    memset(rcvBuffer, 0, 1500);
-    if (recv(mailSocket, rcvBuffer, 1500, 0) == 0)
-    {
-        fprintf(stderr, "fail to receive!\n");
-        exit(-1);
-    }
-
-    printf("connect: %s\n", rcvBuffer);
-
-    // EHLO
-    char localHost[20] = {0};
-    gethostname(localHost, 20);
-    memset(sendBuffer, 0, 1500);
-    strcat(localHost, "-PC\r\n");
-    char ehlo[1500] = {0};
-    strcat(ehlo, "EHLO ");
-    sprintf(sendBuffer, strcat(ehlo,localHost));
-    printf("%s\n", sendBuffer);
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("EHLO: %s\n", rcvBuffer);
-
-    // AUTH LOGIN
-    memset(sendBuffer, 0, 1500);
-    sprintf(sendBuffer, "AUTH LOGIN\r\n");
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    // printf("%s\n", buf);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("AUTH: %s\n", rcvBuffer);
-
-    // USER
-    memset(sendBuffer, 0, 1500);
-    sprintf(sendBuffer, "2643811975"); //你的qq号
-    memset(account, 0, 128);
-    base64(account, sendBuffer, strlen(sendBuffer));
-    sprintf(sendBuffer, "%s\r\n", account);
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    // printf("%s\n", buf);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("user: %s\n", rcvBuffer);
-
-    // PASSWORD
-    sprintf(sendBuffer, "bpgblkyafitiebga"); //使用授权码
-    memset(pass, 0, 128);
-    base64(pass, sendBuffer, strlen(sendBuffer));
-    sprintf(sendBuffer, "%s\r\n", pass);
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    // printf("%s\n", buf);
-
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("password: %s\n", rcvBuffer);
-
-    // MAIL FROM
-    memset(sendBuffer, 0, 1500);
-    sprintf(sendBuffer, "MAIL FROM: <2643811975@qq.com>\r\n");
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("mail from: %s\n", rcvBuffer);
-
-    // RCPT TO 第一个收件人
-    sprintf(sendBuffer, "RCPT TO:<%s>\r\n", email);
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("mail to: %s\n", rcvBuffer);
-
-    // DATA 准备开始发送邮件内容
-    sprintf(sendBuffer, "DATA\r\n");
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("data begin: %s\n", rcvBuffer);
-
-    // 发送邮件内容，\r\n.\r\n内容结束标记
-    sprintf(sendBuffer, "%s\r\n.\r\n", body);
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("data end: %s\n", rcvBuffer);
-
-    // QUIT
-    sprintf(sendBuffer, "QUIT\r\n");
-    send(mailSocket, sendBuffer, strlen(sendBuffer), 0);
-    memset(rcvBuffer, 0, 1500);
-    recv(mailSocket, rcvBuffer, 1500, 0);
-    printf("quit: %s\n", rcvBuffer);
-
-    // VC2005 需要使用
     closesocket(mailSocket);
     WSACleanup();
     return;
---
+++ lab7/proxy.cpp
@@ -0,0 +1,219 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <process.h>
+#include <string.h>
+#include <string>
+
+#pragma comment(lib,"Ws2_32.lib")
+using namespace std;
+#define MAXSIZE 65507 //发送数据报文的最大长度
+#define HTTP_PORT 80 //http 服务器端口
+
+//Http 重要头部数据
+struct HttpHeader {
+	char method[4];//POST 或者 GET
+	char url[1024]; //请求的url
+	char host[1024]; //目标主机
+	char cookie[1024 * 10]; //cookie
+	HttpHeader() {
+		ZeroMemory(this, sizeof(HttpHeader));//内存置零
+	}
+};
+
+//代理相关参数
+SOCKET ProxyServer;//代理服务器socket
+sockaddr_in ProxyServerAddr;//代理地址信息
+const int ProxyPort = 10240;//代理端口
+
+struct ProxyParam {
+	SOCKET clientSocket;
+	SOCKET serverSocket;
+};
+
+bool InitSocket();
+void ParseHttpHead(char* buffer, HttpHeader* httpHead);
+bool ConnectToServer(SOCKET* serverSocket, char* host);
+int ProxyThread(ProxyParam* lpParameter);
+
+int main(int argc, char* argv[]) {
+	printf("代理服务器正在启动\n");
+	printf("初始化...\n");
+	if (!InitSocket()) {//初始化socket
+		printf("socket初始化失败\n");
+		return -1;
+	}
+	printf("代理服务器正在运行，监听端口 %d\n", ProxyPort);
+	SOCKET acceptSocket = INVALID_SOCKET;
+	ProxyParam* lpProxyParam;
+	HANDLE hThread;
+	DWORD dwThreadID;
+	//代理服务器不断监听
+	while (true) {
+		acceptSocket = accept(ProxyServer, NULL, NULL);//监听到socket
+		lpProxyParam = new ProxyParam;
+		if (lpProxyParam == NULL) {
+			continue;
+		}
+		lpProxyParam->clientSocket = acceptSocket;
+		ProxyThread(lpProxyParam);
+		getchar();
+		//Sleep(200);
+	}
+	closesocket(ProxyServer);
+	WSACleanup();
+	return 0;
+}
+
+bool InitSocket() {//初始化socket
+	//加载套接字库（必须）
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	//套接字加载时错误提示
+	int err;
+	//版本2.2
+	wVersionRequested = MAKEWORD(2, 2);
+	//加载dll文件Scoket库
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		//找不到winsock.dll
+		printf("加载winsock失败，错误代码为：%d\n", WSAGetLastError());
+		return false;
+	}
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+		printf("不能找到正确的winsock版本\n");
+		WSACleanup();
+		return false;
+	}
+	ProxyServer = socket(AF_INET, SOCK_STREAM, 0);//创建套接字
+	if (INVALID_SOCKET == ProxyServer) {
+		printf("创建套接字失败，错误代码为：%d\n", WSAGetLastError());
+		return false;
+	}
+	ProxyServerAddr.sin_family = AF_INET;
+	ProxyServerAddr.sin_port = htons(ProxyPort);//将主机字节顺序转化为网络字节顺序，大端
+	ProxyServerAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (bind(ProxyServer, (SOCKADDR*)&ProxyServerAddr, sizeof(SOCKADDR)) == SOCKET_ERROR) {//代理与socket绑定
+		printf("绑定套接字失败\n");
+		return false;
+	}
+	if (listen(ProxyServer, SOMAXCONN) == SOCKET_ERROR) {
+		printf("监听端口%d失败", ProxyPort);
+		return false;
+	}
+	return true;
+}
+
+int ProxyThread(ProxyParam* lpParameter) {//socket响应线程
+	char Buffer[MAXSIZE];
+	char* CacheBuffer;
+	ZeroMemory(Buffer, MAXSIZE);
+	SOCKADDR_IN clientAddr;
+	int length = sizeof(SOCKADDR_IN);
+	INT recvSize;
+	int ret;
+	recvSize = recv(((ProxyParam*)lpParameter)->clientSocket, Buffer, MAXSIZE, 0);//收取数据
+	if (recvSize <= 0) {
+		printf("关闭套接字\n");
+		Sleep(200);
+		closesocket(((ProxyParam*)lpParameter)->clientSocket);
+		closesocket(((ProxyParam*)lpParameter)->serverSocket);
+		return -1;
+	}
+	HttpHeader* httpHeader = new HttpHeader();
+	CacheBuffer = new char[recvSize + 1];
+	ZeroMemory(CacheBuffer, recvSize + 1);
+	memcpy(CacheBuffer, Buffer, recvSize);
+	ParseHttpHead(CacheBuffer, httpHeader);//转换头
+	delete CacheBuffer;
+	if (!ConnectToServer(&((ProxyParam*)lpParameter)->serverSocket, httpHeader->host)) {//代理访问
+		//goto error;
+		printf("关闭套接字\n");
+		Sleep(200);
+		closesocket(((ProxyParam*)lpParameter)->clientSocket);
+		closesocket(((ProxyParam*)lpParameter)->serverSocket);
+		return -1;
+	}
+	printf("代理连接主机 %s 成功\n", httpHeader->host);
+
+	//将客户端发送的HTTP数据报文直接转发给目标服务器
+	ret = send(((ProxyParam*)lpParameter)->serverSocket, Buffer, strlen(Buffer) + 1, 0);
+	//等待目标服务器返回数据
+	recvSize = recv(((ProxyParam*)lpParameter)->serverSocket, Buffer, MAXSIZE, 0);
+	if (recvSize <= 0) {
+		printf("关闭套接字\n");
+		Sleep(200);
+		closesocket(((ProxyParam*)lpParameter)->clientSocket);
+		closesocket(((ProxyParam*)lpParameter)->serverSocket);
+		return -1;
+	}
+	//将目标服务器返回的数据直接转发给客户端
+	ret = send(((ProxyParam*)lpParameter)->clientSocket, Buffer, sizeof(Buffer), 0);
+	return 0;
+}
+
+void ParseHttpHead(char* buffer, HttpHeader* httpHeader) {//转换头
+	char* p;
+	char* ptr;
+	const char* delim = "\r\n";//换行符
+	p = strtok_s(buffer, delim, &ptr);//提取第一行
+	printf("header: %s\n", p);
+	if (p[0] == 'G') {	//GET
+		memcpy(httpHeader->method, "GET", 3);
+		memcpy(httpHeader->url, &p[4], strlen(p) - 13);
+	}
+	else if (p[0] == 'P') {	//POST
+		memcpy(httpHeader->method, "POST", 4);
+		memcpy(httpHeader->url, &p[5], strlen(p) - 14);
+	}
+	printf("%s\n", httpHeader->url);//url
+	p = strtok_s(NULL, delim, &ptr);//使用换行符分割，提取当前行
+	while (p) {
+		switch (p[0]) {
+		case 'H'://HOST
+			memcpy(httpHeader->host, &p[6], strlen(p) - 6);
+			break;
+		case 'C'://Cookie
+			if (strlen(p) > 8) {
+				char header[8];
+				ZeroMemory(header, sizeof(header));
+				memcpy(header, p, 6);
+				if (!strcmp(header, "Cookie")) {
+					memcpy(httpHeader->cookie, &p[8], strlen(p) - 8);
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		p = strtok_s(NULL, delim, &ptr);//使用换行符分割，提取当前行
+	}
+}
+
+bool ConnectToServer(SOCKET* serverSocket, char* host) {//代理访问server
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(HTTP_PORT);
+	printf("****HOST:%s\n", host);
+	if (strcmp("computer.hnust.edu.cn/", host) == 0 || strcmp("computer.hnust.edu.cn", host) == 0)
+	{
+		printf("BLOCK http://computer.hnust.edu.cn/\n");
+		return false;
+	}
+	HOSTENT* hostent = gethostbyname(host);//获取主机名字、地址信息
+
+	if (!hostent) {
+		return false;
+	}
+	in_addr Inaddr = *((in_addr*)*hostent->h_addr_list);
+	serverAddr.sin_addr.s_addr = inet_addr(inet_ntoa(Inaddr));
+	*serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (*serverSocket == INVALID_SOCKET) {
+		return false;
+	}
+	if (connect(*serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {//尝试连接
+		closesocket(*serverSocket);
+		return false;
+	}
+	return true;
+}
